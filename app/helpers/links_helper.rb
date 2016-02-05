1d60deb130a3cfffc0866ab76b6fe2155e25b1b5
cmakoumbou <cmakoumbou@gmail.com>
2016-02-05 10:31:59 +0000

    Image fix 3

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,28 +1,26 @@
 module LinksHelper
   def options_for_default_images
     [
-      ['Blogger', 'blogger'],
-      ['Facebook', 'facebook'],
-      ['Flickr', 'flickr'],
-      ['Google', 'google'],
-      ['Instagram', 'instagram'],
-      ['Linkedin', 'linkedin'],
-      ['Medium', 'medium'],
-      ['Meerkat', 'meerkat'],
-      ['Periscope', 'periscope'],
-      ['Pinterest', 'pinterest'],
-      ['Reddit', 'reddit'],
-      ['Snapchat', 'snapchat'],
-      ['Soundcloud', 'soundcloud'],
-      ['Tumblr', 'tumblr'],
-      ['Twitch', 'twitch'],
-      ['Twitter', 'twitter'],
-      ['Vimeo', 'vimeo'],
-      ['Vine', 'vine'],
-      ['Wordpress', 'wordpress'],
-      ['Youtube', 'youtube']
+      ['Blogger', 'blogger.png'],
+      ['Facebook', 'facebook.png'],
+      ['Flickr', 'flickr.png'],
+      ['Google', 'google.png'],
+      ['Instagram', 'instagram.png'],
+      ['Linkedin', 'linkedin.png'],
+      ['Medium', 'medium.png'],
+      ['Meerkat', 'meerkat.png'],
+      ['Periscope', 'periscope.png'],
+      ['Pinterest', 'pinterest.png'],
+      ['Reddit', 'reddit.png'],
+      ['Snapchat', 'snapchat.png'],
+      ['Soundcloud', 'soundcloud.png'],
+      ['Tumblr', 'tumblr.png'],
+      ['Twitch', 'twitch.png'],
+      ['Twitter', 'twitter.png'],
+      ['Vimeo', 'vimeo.png'],
+      ['Vine', 'vine.png'],
+      ['Wordpress', 'wordpress.png'],
+      ['Youtube', 'youtube.png']
     ]
   end
 end
-
-
